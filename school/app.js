84289fe45c2392760327c35e1a6db7dce62d2363
CITRITE\junshengc <junsheng.cao@citrix.com>
2018-04-19 20:43:59 +0800

    12

--- school/app.js
+++ school/app.js
@@ -33,6 +33,50 @@ App({
       }
     })
   },
+
+  tapInnerLinkHandler: function (event) {
+    let param = event.currentTarget.dataset.eventParams,
+      pageRoot = {
+        'groupCenter': '/eCommerce/pages/groupCenter/groupCenter',
+        'shoppingCart': '/eCommerce/pages/shoppingCart/shoppingCart',
+        'myOrder': '/eCommerce/pages/myOrder/myOrder',
+      };
+    if (param) {
+      param = JSON.parse(param);
+      let pageLink = param.inner_page_link;
+      let url = pageRoot[pageLink] ? pageRoot[pageLink] : '/pages/' + pageLink + '/' + pageLink;
+      if (url.indexOf('/prePage/') >= 0) {
+        this.turnBack();
+      } else if (url) {
+        let is_redirect = param.is_redirect == 1 ? true : false;
+        this.turnToPage(url, is_redirect);
+      }
+    }
+  },
+  
+  turnToPage: function (url, isRedirect) {
+    let tabBarPagePathArr = this.getTabPagePathArr();
+    if (this.globalData.turnToPageFlag) return;
+    this.globalData.turnToPageFlag = true;
+    setTimeout(() => {
+      this.globalData.turnToPageFlag = false;
+    }, 1000)
+    if (tabBarPagePathArr.indexOf(url) != -1) {
+      this.switchToTab(url);
+      return;
+    }
+    if (!isRedirect) {
+      wx.navigateTo({
+        url: url
+      });
+    } else {
+      wx.redirectTo({
+        url: url
+      });
+    }
+  },
+
+
   globalData: {
     userInfo: null
   }
