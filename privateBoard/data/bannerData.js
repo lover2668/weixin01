f90650e5f96deadc62f348ad5265af5cfc444b63
CITRITE\junshengc <junsheng.cao@citrix.com>
2018-08-18 11:28:26 +0800

    private board small weixin app

---
+++ privateBoard/data/bannerData.js
@@ -0,0 +1,21 @@
+var bannerCarousel = {
+  "banner": {
+    "indicatorDots": true,
+    "autoplay": true,
+    "interval": 2000,
+    "duration": 800,
+    "circular": true,
+    "content": [
+      {
+        "pic": "http:\/\/img.weiye.me\/zcimgdir\/album\/file_58ef9256671fa.png",
+      },
+      {
+        "pic": "http:\/\/img.weiye.me\/zcimgdir\/album\/file_58ef91634e754.png",
+      }
+    ],
+  },
+}
+
+module.exports = {
+  bannerCarousel: bannerCarousel
+}
---
+++ privateBoard/data/sortData.js
@@ -0,0 +1,39 @@
+var sortAlbum = {
+  "album": {
+    "sortData": "",
+    "color": "",
+    "style": "background-color:rgb(255, 255, 255);font-size:28.125rpx;opacity:1;text-align:center;margin-left:auto;",
+    "ul_style": "padding-left:82.03125rpx;padding-top:23.4375rpx;margin-left:auto;",
+    "content": [
+      {
+        "title": "简介",
+        "pic": "http:\/\/img.weiye.me\/zcimgdir\/album\/file_58f05797a81e3.png",
+        "li_style": "width:84.9609375rpx;margin-right:82.03125rpx;margin-bottom:23.4375rpx;margin-left:auto;",
+        "img_style": "height:84.9609375rpx;border-radius:50%;margin-left:auto;",
+      },
+      {
+        "title": "课程",
+        "pic": "http:\/\/img.weiye.me\/zcimgdir\/album\/file_58f0579b77205.png",
+        "li_style": "width:84.9609375rpx;margin-right:82.03125rpx;margin-bottom:23.4375rpx;margin-left:auto;",
+        "img_style": "height:84.9609375rpx;border-radius:50%;margin-left:auto;",
+      },
+      {
+        "title": "活动",
+        "pic": "http:\/\/img.weiye.me\/zcimgdir\/album\/file_58f0579fe81dc.png",
+        "li_style": "width:84.9609375rpx;margin-right:82.03125rpx;margin-bottom:23.4375rpx;margin-left:auto;",
+        "img_style": "height:84.9609375rpx;border-radius:50%;margin-left:auto;",
+      },
+
+      {
+        "title": "购物车",
+        "pic": "http:\/\/img.weiye.me\/zcimgdir\/album\/file_58f057a2ade02.png",
+        "li_style": "width:84.9609375rpx;margin-right:82.03125rpx;margin-bottom:23.4375rpx;margin-left:auto;",
+        "img_style": "height:84.9609375rpx;border-radius:50%;margin-left:auto;",
+      }
+    ],
+  },
+}
+
+module.exports = {
+  sortAlbum: sortAlbum
+}
