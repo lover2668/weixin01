8e3f7011cbda6ed012599cc8717a9005dbc64c69
lover2668@163.com <lover2668@163.com>
2018-11-01 10:38:17 +0800

    add log for lab

--- lab-weapp-client-master/pages/websocket/websocket.js
+++ lab-weapp-client-master/pages/websocket/websocket.js
@@ -28,11 +28,15 @@ Page({
       hintLine2: '...'
     });
     this.listen();
+    console.log("config.host:", config.host)
     wafer.setLoginUrl(`https://${config.host}/login`);
     wafer.login({
       success: () => {
         const header = wafer.buildSessionHeader();
+        console.log("header:", header)
         const query = Object.keys(header).map(key => `${key}=${encodeURIComponent(header[key])}`).join('&');
+        console.log("query:", query)
+        console.log("this.data.url:", this.data.url)
         wx.connectSocket({
           // 小程序 wx.connectSocket() API header 参数无效，把会话信息附加在 URL 上
           url: `${this.data.url}?${query}`,
@@ -40,6 +44,7 @@ Page({
         });
       },
       fail: (err) => {
+        console.log("err:", err)
         this.setData({
           status: 'warn',
           connecting: false,
@@ -54,7 +59,9 @@ Page({
    * 监听 WebSocket 事件
    */
   listen() {
+    console.log("start listen...")
     wx.onSocketOpen(() => {
+      console.log("start socket open...")
       this.socketOpen = true;
       this.setData({
         status: 'success',
@@ -65,12 +72,15 @@ Page({
       console.info('WebSocket 已连接');
     });
     wx.onSocketMessage((message) => {
+      console.log("start socket message...")
+      console.log("message:", message)
       this.setData({
         hintLine2: message.data
       });
       lab.finish('websocket');
     });
     wx.onSocketClose(() => {
+      console.log("start socket close...")
       this.setData({
         status: 'waiting',
         hintLine1: 'WebSocket 已关闭'
@@ -78,6 +88,7 @@ Page({
       console.info('WebSocket 已关闭');
     });
     wx.onSocketError(() => {
+      console.log("start socket error...")
       setTimeout(() => {
         this.setData({
           status: 'warn',
@@ -94,6 +105,7 @@ Page({
    * 发送一个包含当前时间信息的消息
    */
   send() {
+    console.log("start send...")
     wx.sendSocketMessage({
       data: new Date().toTimeString().split(' ').shift() + '.' + (new Date().getMilliseconds())
     });
@@ -103,6 +115,7 @@ Page({
    * 关闭 WebSocket 连接
    */
   close() {
+    console.log("start close...")
     this.socketOpen = false;
     wx.closeSocket();
   }
